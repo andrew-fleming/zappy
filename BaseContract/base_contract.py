9460e75486bdf5835f2668496e14c139501e13b0
andrew <fleming-andrew@protonmail.com>
2021-02-05 03:58:45 -0500

    Started populating BaseContract class and getAddress function

--- BaseContract/base_contract.py
+++ BaseContract/base_contract.py
@@ -1,10 +1,51 @@
 import json
 import os
+from web3 import Web3
 
-def _load_abi(name: str) -> str:
-    path = f"{os.path.dirname(os.path.dirname(__file__))}\packages\\artifacts/"
+infura = 'https://kovan.infura.io/v3/afaba81a13fd484492da91b0baeb10d0'
+w3 = Web3(Web3.HTTPProvider(infura))
+#print(w3.isConnected())
+
+
+
+
+def load_json(name: str) -> str:
+    path = f"{os.path.dirname(os.path.dirname(__file__))}/packages/artifacts/"
     with open(os.path.abspath(path + f"{name}.json")) as f:
         json_file = json.load(f)
-        abi = json_file['abi']
+    return json_file
+
+def load_abi(name: str) -> dict:
+    obj = load_json(name)
+    abi = obj['abi']
     return abi
+
+def load_address(name: str) -> dict:
+    a_dict = load_json(name)
+    addr = a_dict[]
+
+
+print(load_abi("Arbiter"))
+
+#def load_address(address: str) -> str:
+
+
+
+
+
+class BaseContract:
+    def __init__(
+        self,
+        provider: any,
+        web3: any,
+        contract: any,
+        networkId: int,
+        coordinator: any,
+        artifact: any,
+        name: str,
+        address: str or None
+    ) -> None:
+        print('foo')
+
+
 
